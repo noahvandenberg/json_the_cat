a04574143b9e2c65d1e6483f491757a4460924d7
Noah Vandenberg <noah@noahvandenberg.com>
2022-01-20 14:28:37 -0800

    linted repo

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,15 @@
 const request = require('request');
-const args = process.argv.slice('2');
 
 const fetchBreedDescription = (breedName, callback) => {
+
   request(`https://api.thecatapi.com/v1/breeds/search?name=${breedName}`, (error, response, body) => {
 
     if (error !== null) return callback(error);
     if (JSON.parse(body)[0] === undefined) return callback(`${breedName} not found`, undefined);
-    return callback(undefined,JSON.parse(body)[0].description)
-    
+    return callback(undefined,JSON.parse(body)[0].description);
+
   });
+  
 };
 
 
--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const { fetchBreedDescription } = require("./breedFetcher");
 const breedName = process.argv[2];
 
 fetchBreedDescription(breedName, (error, description) => {
-  if (error) {console.log("Error fetch details:", error); return};
-  console.log(description)
+  if (error) {
+    console.log("Error fetch details:", error);
+    return;
+  }
+  console.log(description);
 });
